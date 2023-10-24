ccd00c2843b73516632dc7c7693f923a5a1fdb12
ekaterinarogova <ekatersergeev@gmail.com>
2023-10-24 20:04:21 +0300

    created models of company, contacts and products,

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'sales.apps.SalesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sales',
+        'USER': 'postgres',
+        'PASSWORD': '451501',
     }
 }
 
@@ -103,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -121,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Company(models.Model):
+    TYPE_CHOICES = (
+        ('FA', 'Factory'),
+        ('RN', 'Retail network'),
+        ('IE', 'Individual entrepreneur')
+    )
+    title = models.CharField(max_length=200, verbose_name='название')
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES, verbose_name='тип поставщика')
+    supplier = models.ForeignKey('self', on_delete=models.SET_NULL, null=True,
+                                 verbose_name='поставщик', related_name='company')
+    debt = models.PositiveIntegerField(verbose_name='задолженность поставщику')
+    created_at = models.DateTimeField(verbose_name='время создания', auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.title}"
+
+    class Meta:
+        verbose_name = 'компания'
+        verbose_name_plural = 'компании'
+
+
+class Contacts(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='компания', related_name='contacts')
+    email = models.EmailField(verbose_name='email')
+    country = models.CharField(max_length=150, verbose_name='страна')
+    city = models.CharField(max_length=150, verbose_name='город')
+    street = models.CharField(max_length=150, verbose_name='улица')
+    house = models.CharField(max_length=100, verbose_name='дом')
+
+    def __str__(self):
+        return f"{self.email}, {self.country}, {self.street}, {self.city}, {self.street}, {self.house}"
+
+    class Meta:
+        verbose_name = 'контакты'
+        verbose_name_plural = 'контакты'
+
+
+class Product(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='компания', related_name='product')
+    title = models.CharField(max_length=200, verbose_name='название')
+    model = models.CharField(max_length=150, verbose_name='модель')
+    release_date = models.DateField(verbose_name='дата выхода продукта', default=timezone.now())
+
